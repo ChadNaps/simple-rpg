248084d1c574ab4791771e30871802ad33a94ccb
Chad Napper <contact@chadnaps.com>
2022-08-14 13:14:37 -0600

    removes unneeded comments

--- main.js
+++ main.js
@@ -26,18 +26,6 @@ let saveData = {
 	}
 };
 
-// rl.question("What is your name ? ", function(name) {
-//     rl.question("Where do you live ? ", function(country) {
-//         console.log(`${name}, is a citizen of ${country}`);
-//         rl.close();
-//     });
-// });
-
-// rl.on("close", function() {
-//     console.log("\nBYE BYE !!!");
-//     process.exit(0);
-// });
-
 function quit(interfaceToQuit, code) {
 	interfaceToQuit.on("close", (code) => { process.exit(code); });
 }
