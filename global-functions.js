36f1edd90fa23bf369d34256723000f2e34fea4f
Chad Napper <contact@chadnaps.com>
2022-08-19 19:52:24 -0600

    initial refactor - code is broken

--- global-functions.js
+++ global-functions.js
@@ -1,23 +1,35 @@
-const readline = require('readline');
+const tryAgainMessage = "Invalid selection, please try again: ";
 
 exports.dramaticElipses = function (message, times, intervalInMs, iostream) {
 	iostream.write(message);
 	waitAndDo(times);
-
 	function waitAndDo(times) {
 		if(times < 1) {
 			iostream.write('\n');
 			return;
 		}
-
 		setTimeout(function() {
-			iostream.write('.');
+				iostream.write('.');
 
-			waitAndDo(times-1);
-		}, intervalInMs);
+				waitAndDo(times-1);
+				}, intervalInMs);
 	}
 }
 
 exports.quit = function (interfaceToQuit, code) {
-  interfaceToQuit.on("close", (code) =>     { process.exit(code); });
+	interfaceToQuit.on("close", (code) => { console.clear(); process.exit(code); });
+}
+
+// TODO - change nested questions to promises: https://stackoverflow.com/questions/36540996/how-to-take-two-consecutive-input-with-the-readline-module-of-node-js
+exports.prompt = function (iostream, acceptableResponses = [-1], message = tryAgainMessage) {
+	if (!Array.isArray(acceptableResponses)) {
+		throw new Error("Second parameter MUST be an array of acceptable responses.");
+	}
+	iostream.question(message, (selection) => {
+		if (typeof acceptableResponses[selection] !== "undefined") {
+			console.log("It's here!");
+		} else {
+			exports.prompt();
+		}
+	});
 }
--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const isDevMode = true;
 
 const readline = require('readline');
-const { dramaticElipses, quit } = require('./global-functions.js');
+const { dramaticElipses, quit, prompt } = require('./global-functions.js');
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-const tryAgainMessage = "Invalid selection, please try again: ";
 
 const saveData = {
 	exists: false,
@@ -29,37 +28,34 @@ const saveData = {
 };
 
 function mainMenu() {
-	const welcomeMessage = 
-	"Welcome to the Playground \n\
+	const welcomeMessage = "Welcome to my game. I hope you enjoy yourself. \n\
 Please select an option: \n\
 		 1. New Game \n\
 		 2. Load Game \n\
 		 3. Quit \n";
 	const todoMessage = "This feature is still in development. Please select another: ";
-
 	console.clear();
-	prompt(welcomeMessage);
-
-	function prompt(message = tryAgainMessage) {
-		rl.question(message, (selection) => {
-			const descendText = "Good luck adventurer. You begin your descent";
-			if (selection === '1') {
-				console.clear();
-				if (isDevMode)
-					playIntro(); // TODO - Remove if/else before launch
-				else {
-					dramaticElipses(descendText, 5, 750, rl);
-					setTimeout(playIntro, 800*5);
-				}
-			} else if (selection === '2') {
-				prompt(todoMessage);
-			} else if (selection === '3') {
-				rl.close();
-			} else {
-				prompt();
-			}
-		});
-	}
+	prompt(rl, ["New Game", "Load Game", "Quit"], welcomeMessage);
+	// function prompt(message = tryAgainMessage) {
+//	rl.question(message, (selection) => {
+//		const descendText = "Good luck adventurer. You begin your descent";
+//		if (selection === '1') {
+//			console.clear();
+//			if (isDevMode)
+//				playIntro(); // TODO - Remove if/else before launch
+//			else {
+//				dramaticElipses(descendText, 5, 750, rl);
+//				setTimeout(playIntro, 800*5);
+//			}
+//		} else if (selection === '2') {
+//			prompt(todoMessage);
+//		} else if (selection === '3') {
+//			rl.close();
+//		} else {
+//			prompt();
+//		}
+//	});
+//}
 }
 
 function playIntro() {
